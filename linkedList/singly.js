8f55b0dd23a497c6d62b4967a3b364cf745384dc
Rubén Esparza <rubenesparzasoto95@gmail.com>
2021-04-09 00:04:55 -0600

    chapter(linkedList): add ours nodes

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -32,4 +32,30 @@ class SinglyLinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
+    return this;
+  }
 }
+
+let mySinglyLinkedList = new SinglyLinkedList(1);
+
+mySinglyLinkedList.append(2);
+mySinglyLinkedList.append(3);
+mySinglyLinkedList.prepend(0);
