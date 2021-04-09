dad2562d42b69df8f62c3b7393d657e7e78edd60
Rubén Esparza <rubenesparzasoto95@gmail.com>
2021-04-09 15:43:13 -0600

    chapter(linkedList): add middle nodes

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -1,23 +1,23 @@
 // 1 --> 2 --> 3 --> 4 --> 5 --> null
 
-const exampleSinglyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: {
-            value: 5,
-            next: null,
-          },
-        },
-      },
-    },
-  },
-};
+// const exampleSinglyLinkedList = {
+//   head: {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: {
+//             value: 5,
+//             next: null,
+//           },
+//         },
+//       },
+//     },
+//   },
+// };
 
 class Node {
   constructor(value) {
@@ -52,6 +52,33 @@ class SinglyLinkedList {
 
     return this;
   }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const previousPointer = this.getPointerByIndex(index - 1);
+
+    newNode.next = previousPointer.next;
+    previousPointer.next = newNode;
+    this.length++;
+
+    return this;
+  }
+
+  getPointerByIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
 }
 
 let mySinglyLinkedList = new SinglyLinkedList(1);
@@ -59,3 +86,4 @@ let mySinglyLinkedList = new SinglyLinkedList(1);
 mySinglyLinkedList.append(2);
 mySinglyLinkedList.append(3);
 mySinglyLinkedList.prepend(0);
+mySinglyLinkedList.insert(2, 4);
